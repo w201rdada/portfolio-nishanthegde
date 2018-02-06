4460e7e888b62516060358e48276c88c26fb6646
nishant hegde <nhegde@netflix.com>
2018-02-05 22:10:22 -0800

    deploy liquid hot magma3

--- _deploy.sh
+++ _deploy.sh
@@ -17,7 +17,7 @@ Disallow: /" > docs/robots.txt
 
 # site will be public so dissuade robots and search engines from crawling your page
 git add . \
-&& git commit -m "deploy liquid hot magma2" || true \
+&& git commit -m "deploy liquid hot magma3" || true \
 && echo '
 
 Ready to push with authorization.
