9bc7f6b419d789527e6a176a5831273aba25146c
w201rdada-portfolio-nishanthegde-likisnxk <w201rdada-portfolio-nishanthegde-likisnxk@local>
2018-02-06 02:44:15 +0000

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # put your info here and uncomment so git (and GitHub) knows who your commits belong to!
-# git config user.email "calnet@berkeley.edu"
-# git config user.name "Oski Bear"
+# git config user.email "nhegde@berkeley.edu"
+# git config user.name "Nishant Hegde"
 
 # make sure you're on the master branch
 git checkout master
